3ffcca261b004667a2e7907a3da325f3e7bf4f7a
Pedro Maciel <pedrozath@gmail.com>
2013-03-12 17:55:12 -0300

    update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eventoscte::Application.config.secret_token = '642eaaeb04b478adace528756e344cb0949aaf1b523abf02800a41c0444f518adf4f9e450eb02ccdd88d53931a645325a6b53e6a67acaf1022a703f65f905a52'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Eventoscte::Application.routes.draw do
+  root :to => "pages#index"
+end
