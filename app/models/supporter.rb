8d70993e9c9d043f855c53180a335f0ff9b548d4
Pedro Maciel <pedrozath@gmail.com>
2013-04-30 11:46:31 -0300

    patrocinadores na ordem correta

--- app/models/supporter.rb
+++ app/models/supporter.rb
@@ -1,4 +1,6 @@
 class Supporter < ActiveRecord::Base
   attr_accessible :image, :name, :image_cache, :remove_image
   mount_uploader :image, SupporterLogoUploader
+
+  default_scope order("name ASC")
 end
