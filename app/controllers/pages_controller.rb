4241a298adb2f6dc853b19f57876634d1112445d
Pedro Maciel <pedrozath@gmail.com>
2013-04-30 11:46:31 -0300

    patrocinadores na ordem correta

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,13 @@
 class PagesController < ApplicationController
 	def index
 		@sliders = Slider.all
+		@supporters = Supporter.all
 	end
 
 	def local
+	end
+
+	def sponsors
+		@supporters = Supporter.all
 	end
 end
