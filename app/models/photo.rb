5e40979469af98d0b04cd4bed8569470d3e20f77
Pedro Maciel <pedrozath@gmail.com>
2013-05-18 17:45:16 -0300

    fotos por ordem de criação

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,8 +1,9 @@
 class Photo < ActiveRecord::Base
+	default_scope lambda { order :created_at }
 	paginates_per 7
 	attr_accessible :image, :event_id, :image_cache, :remove_image
 	belongs_to :event
 	mount_uploader :image, PhotoUploader
 
-	validates :image, presence: { message: "Envie uma imagem (ou apague a foto completamente)"}
+	validates :image, presence: { message: "Envie uma imagem (ou apague a foto completamente)" }
 end
