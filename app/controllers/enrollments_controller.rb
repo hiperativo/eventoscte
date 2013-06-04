098e0bd6e9f8257d2a460f23ba93e89eae988c51
Pedro Maciel <pedrozath@gmail.com>
2013-06-04 14:53:49 -0300

    correção do commit anterior

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -87,7 +87,7 @@ class EnrollmentsController < ApplicationController
 															cep_do_sacado: @enrollment.cep,
 															cidade_do_sacado: @enrollment.city,
 															estado_do_sacado: @enrollment.state,
-															data_de_vencimento: (Time.new 2013, 6, 6, 8) }
+															data_de_vencimento: Time.new(2013, 6, 6, 8) })
 															# data_de_vencimento: Time.now + 5.days })
 
 				@enrollment.update_attribute(:itau_crypto, @itau_crypto)
