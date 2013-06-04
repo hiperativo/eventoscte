e732374d7513c9cb67dbace3e5d01869741b7c32
Pedro Maciel <pedrozath@gmail.com>
2013-06-03 21:09:37 -0300

    updates
    foi adicionado o campo Materia Link, onde vai o link da matéria. Se o
    clipping tiver um arquivo (no campo matéria), o link vai pro arquivo.
    Se não tiver, vai pro Matéria Link (link da matéria).
    
    OBS: O link da matéria deve incluir http:// se estiver indo para um
    endereço externo.

--- app/models/clipping.rb
+++ app/models/clipping.rb
@@ -1,6 +1,6 @@
 class Clipping < ActiveRecord::Base
   attr_accessible :desc, :lead, :materia, :titulo, 
-  :materia_cache, :remove_materia,
+  :materia_cache, :remove_materia, :materia_link, 
   :image, :image_cache, :remove_image
   mount_uploader :materia, ClippingUploader
   mount_uploader :image, ClippingImageUploader
