bf263533ae78e6eca92c1b852c8258dca7f00204
Pedro Maciel <pedrozath@gmail.com>
2013-06-19 21:48:21 -0300

    updates

--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -1,6 +1,7 @@
 class Speaker < ActiveRecord::Base
-  attr_accessible :avatar, :avatar_cache, :remove_avatar, :bio, :name, :ocupation
-  has_many :talks
-  # has_and_belongs_to_many :events
-  mount_uploader :avatar, AvatarUploader
+	default_scope lambda { where("length(bio) > 0")}
+	attr_accessible :avatar, :avatar_cache, :remove_avatar, :bio, :name, :ocupation
+	has_many :talks
+	# has_and_belongs_to_many :events
+	mount_uploader :avatar, AvatarUploader
 end
