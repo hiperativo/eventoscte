06d39b3d4ba4e5280cb79e8f46124a843c5ee0b1
Pedro Maciel <pedrozath@gmail.com>
2013-06-27 00:45:44 -0300

    Revert "instalei rails"
    This reverts commit 2f1930a1803a6115cbe4bb3375fae9b53250c633.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130624235533) do
+ActiveRecord::Schema.define(:version => 20130618203502) do
 
   create_table "admin_users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -126,7 +126,6 @@ ActiveRecord::Schema.define(:version => 20130624235533) do
     t.datetime "updated_at", :null => false
     t.integer  "event_id"
     t.integer  "position"
-    t.text     "caption"
   end
 
   create_table "projects", :force => true do |t|
