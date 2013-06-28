ff7cc99213fd175ecb98b07247ac2a6c3dd877ec
Pedro Maciel <pedrozath@gmail.com>
2013-06-28 15:54:57 -0300

    rails 4.0 working

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
+Eventoscte::Application.config.secret_key_base = '642eaaeb04b478adace528756e344cb0949aaf1b523abf02800a41c0444f518adf4f9e450eb02ccdd88d53931a645325a6b53e6a67acaf1022a703f65f905a52'
 Eventoscte::Application.config.secret_token = '642eaaeb04b478adace528756e344cb0949aaf1b523abf02800a41c0444f518adf4f9e450eb02ccdd88d53931a645325a6b53e6a67acaf1022a703f65f905a52'
