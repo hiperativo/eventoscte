371ba0fc7bbfa7708579944ad9895808e1524990
Carolina Toledo <carol@nomedarosa.com.br>
2013-07-01 11:47:27 -0300

    alguma coisa
    alguma coisa

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130624235533) do
+ActiveRecord::Schema.define(version: 20130618203502) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "admin_users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -126,7 +129,6 @@ ActiveRecord::Schema.define(version: 20130624235533) do
     t.datetime "updated_at", null: false
     t.integer  "event_id"
     t.integer  "position"
-    t.text     "caption"
   end
 
   create_table "projects", force: true do |t|
