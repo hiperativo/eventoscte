471b9c01f139b776c8c9403dcc0fd13b0370a494
Pedro Maciel <pedrozath@gmail.com>
2013-07-01 19:04:28 -0300

    voltando

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,7 +3,7 @@ class Photo < ActiveRecord::Base
 	paginates_per 7
 	attr_accessible :image, :event_id, :image_cache, :remove_image, :position
 	belongs_to :event
-	# mount_uploader :image, PhotoUploader
+	mount_uploader :image, PhotoUploader
 
 	validates :image, presence: { message: "Envie uma imagem (ou apague a foto completamente)" }
 end
